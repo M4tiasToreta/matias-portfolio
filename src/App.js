a250c0bd40f39b8e3e556d2f83765f5a432c33b1
M4tiasToreta <toretamatias218@gmail.com>
2023-08-06 16:52:11 -0300

    adding home button, but it still needs refactoring

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
         <p>Hi, I'm Matias</p>
       </div>
       <div className='sidebar'>
-        
+        <svg id='home-button' xmlns="http://www.w3.org/2000/svg" width="24" height="23" viewBox="0 0 24 23" fill="none">
+        <path d="M9.6 23V14.8824H14.4V23H20.4V12.1765H24L12 0L0 12.1765H3.6V23H9.6Z" fill="white"/>
+        </svg>
       </div>
     </>
   );
